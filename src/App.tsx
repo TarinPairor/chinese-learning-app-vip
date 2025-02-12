da1c5bc152e0979028eadec09477a45d899bf8dc
TarinPairor <111269625+TarinPairor@users.noreply.github.com>
2025-02-12 17:43:02 +0800

    fix build issue

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import viteLogo from "/vite.svg";
 import { ElevenLabsClient } from "elevenlabs";
 import "./App.css";
 
-async function App() {
+function App() {
   const [count, setCount] = createSignal(0);
   const client = new ElevenLabsClient();
 
-  await client.textToSpeech.convert(import.meta.env.VITE_SOME_KEY, {
-    output_format: "mp3_44100_128",
-    text: "The first move is what sets everything in motion.",
-    model_id: "eleven_multilingual_v2",
-  });
+  const res = async () =>
+    client.textToSpeech.convert(import.meta.env.VITE_SOME_KEY, {
+      output_format: "mp3_44100_128",
+      text: "The first move is what sets everything in motion.",
+      model_id: "eleven_multilingual_v2",
+    });
+  console.log(res);
 
   return (
     <>
